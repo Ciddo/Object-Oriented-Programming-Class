db3e62417575443dcf13f5bc4cf850f3d8f80cec
Ciddo <cid.dominique@gmail.com>
2015-05-20 21:37:08 -0400

    Homework 9 Final Rev

--- Homework 9/Database.java
+++ Homework 9/Database.java
@@ -11,11 +11,9 @@ a database, not fancy SQL queries.
 import java.sql.*;
 
 public class Database {
-   // JDBC driver name and database URL
    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
    static final String DB_URL = "jdbc:mysql://localhost/EMP";
 
-   //  Database credentials
    static final String USER = "username";
    static final String PASS = "password";
    
@@ -23,29 +21,26 @@ public class Database {
       Connection conn = null;
       Statement stmt = null;
       try{
-            //STEP 2: Register JDBC driver
             Class.forName("com.mysql.jdbc.Driver");
    
-            //STEP 3: Open a connection
             System.out.println("Connecting to database...");
             conn = DriverManager.getConnection(DB_URL,USER,PASS);
 
-            //STEP 4: Execute a query
+
             System.out.println("Creating statement...");
             stmt = conn.createStatement();
             String sql;
             sql = "SELECT id, first, last, age FROM Employees";
             ResultSet rs = stmt.executeQuery(sql);
    
-            //STEP 5: Extract data from result set
+
             while(rs.next()){
-               //Retrieve by column name
+
                int id  = rs.getInt("id");
                int age = rs.getInt("age");
                String first = rs.getString("first");
                String last = rs.getString("last");
    
-               //Display values
                System.out.print("ID: " + id);
                System.out.print(", Age: " + age);
                System.out.print(", First: " + first);
